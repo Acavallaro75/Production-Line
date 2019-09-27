cfb4aafb12ba8c5f084b8fe5bad5c5c516f06ca6
Andrew Cavallaro <47667938+Acavallaro75@users.noreply.github.com>
2019-09-27 09:20:36 -0400

    Update 9/27 with JavaDoc

--- src/production/ProductionLine.java
+++ src/production/ProductionLine.java
@@ -6,15 +6,27 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+/**
+ * The ProductionLine class holds the start method for the JavaFX project. ProductionLine is a
+ * subclass that extends the superclass Application and derives its methods and fields.
+ *
+ * @author Andrew Cavallaro
+ */
 public class ProductionLine extends Application {
+  /**
+   * The parent root will link with the production_line.fxml file to receive what will be displayed
+   * on the production Stage. The title of the stage is currently set to "Product Tracking System."
+   * The scene will originally be displayed at a width of 550 pixels and a height of 500 pixels.
+   */
   @Override
-  public void start(Stage primaryStage) throws Exception {
+  public void start(Stage production) throws Exception {
     Parent root = FXMLLoader.load(getClass().getResource("production_line.fxml"));
-    primaryStage.setTitle("Product Tracking System");
-    primaryStage.setScene(new Scene(root, 550, 500));
-    primaryStage.show();
+    production.setTitle("Product Tracking System");
+    production.setScene(new Scene(root, 550, 500));
+    production.show();
   }
 
+  /** This is the main method that launches any arguments being passed. */
   public static void main(String[] args) {
     launch(args);
   }
